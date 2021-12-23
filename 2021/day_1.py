27ef8efacdf82834f39da2fa9ee1e07f35ea79c5
Sean Rose <sean.s.rose@gmail.com>
2021-12-23 14:46:59 -0800

    Solve 2021 day 1.

---
+++ 2021/day_1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+
+
+def count_depth_increases(depths: list[int]) -> int:
+    depth_increases_count = 0
+    previous_depth = None
+
+    for depth in depths:
+        if previous_depth is not None and depth > previous_depth:
+            depth_increases_count += 1
+        previous_depth = depth
+
+    return depth_increases_count
+
+
+if __name__ == '__main__':
+    with open(Path(__file__).parent / 'day_1_input.txt') as file:
+        depths = [int(line) for line in file.readlines()]
+
+    print(f"Depth increases:  {count_depth_increases(depths)}")
