16442542d8dbece636b6cc19d90ca1d61e627575
Sean Rose <sean.s.rose@gmail.com>
2021-12-23 14:47:33 -0800

    Solve 2021 day 1 part 2.

--- 2021/day_1.py
+++ 2021/day_1.py
@@ -15,8 +15,19 @@ def count_depth_increases(depths: list[int]) -> int:
     return depth_increases_count
 
 
+def count_sliding_window_depth_increases(depths: list[int]) -> int:
+    depth_increases_count = 0
+
+    for index in range(3, len(depths)):
+        if sum(depths[index-2:index+1]) > sum(depths[index-3:index]):
+            depth_increases_count += 1
+
+    return depth_increases_count
+
+
 if __name__ == '__main__':
     with open(Path(__file__).parent / 'day_1_input.txt') as file:
         depths = [int(line) for line in file.readlines()]
 
     print(f"Depth increases:  {count_depth_increases(depths)}")
+    print(f"Sliding window depth increases:  {count_sliding_window_depth_increases(depths)}")
