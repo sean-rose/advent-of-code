910c79971498c735d95d31ad8ae1151632bbf6f9
Sean Rose <sean.s.rose@gmail.com>
2021-12-23 15:06:55 -0800

    Solve 2021 day 2 part 2.

--- 2021/day_2.py
+++ 2021/day_2.py
@@ -24,13 +24,40 @@ def move_submarine(commands: list[str]) -> tuple[int, int]:
     return horizontal_position, depth
 
 
+def move_submarine_2(commands: list[str]) -> tuple[int, int]:
+    aim = 0
+    horizontal_position = 0
+    depth = 0
+
+    for command in commands:
+        move, amount = command.split()
+        if move == 'forward':
+            horizontal_position += int(amount)
+            depth += aim * int(amount)
+        elif move == 'down':
+            aim += int(amount)
+        elif move == 'up':
+            aim -= int(amount)
+        else:
+            raise Exception(f"Unknown command:  {command}")
+
+    return horizontal_position, depth
+
+
 if __name__ == '__main__':
     with open(FILE_PATH.parent / f'{FILE_PATH.stem}_input.txt') as file:
         commands = file.readlines()
 
+    print("--- Part One ---")
     horizontal_position, depth = move_submarine(commands)
     multiplied = horizontal_position * depth
+    print(f"Horizontal position:  {horizontal_position}")
+    print(f"Depth:  {depth}")
+    print(f"Multiplied:  {multiplied}")
 
+    print("--- Part Two ---")
+    horizontal_position, depth = move_submarine_2(commands)
+    multiplied = horizontal_position * depth
     print(f"Horizontal position:  {horizontal_position}")
     print(f"Depth:  {depth}")
     print(f"Multiplied:  {multiplied}")
