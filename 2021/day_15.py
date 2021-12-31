4ca11c9f94d06d410a6a9eee8254f4b0fd5f998c
Sean Rose <sean.s.rose@gmail.com>
2021-12-31 14:05:21 -0800

    Solve 2021 day 15 part 2.

--- 2021/day_15.py
+++ 2021/day_15.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
-from collections import deque
+from __future__ import annotations
+from collections import OrderedDict
 from pathlib import Path
+from typing import Optional
 
 
 FILE_PATH = Path(__file__)
@@ -19,71 +21,117 @@ class Point:
         self.y = y
         self.risk_level = risk_level
 
-    def __hash__(self) -> int:
-        return hash((self.x, self.y))
-
     def __str__(self) -> str:
         return f"({self.x},{self.y})+{self.risk_level}"
 
 
-class Path:
-    def __init__(self, points: list[Point]) -> None:
-        self.points = points
-        self.risk_level = sum(point.risk_level for point in points[1:])
+class PathPoint:
+    def __init__(self, point: Point, previous_path_point: Optional[PathPoint] = None) -> None:
+        self.point = point
+        self.previous_path_point = previous_path_point
+        self.path_risk_level = 0
+        if previous_path_point:
+            self.path_risk_level = previous_path_point.path_risk_level + point.risk_level
 
     def __str__(self) -> str:
-        lines = [str(self.points[0])]
-        cumulative_risk_level = 0
-        for point in self.points[1:]:
-            cumulative_risk_level += point.risk_level
-            lines.append(f"{point} = {cumulative_risk_level}")
-        return '\n'.join(lines)
+        path_traceback = []
+        path_point = self
+        while path_point:
+            path_traceback.append(f"{path_point.point} = {path_point.path_risk_level}")
+            path_point = path_point.previous_path_point
+        
+        return '\n'.join(reversed(path_traceback))
 
 
-def get_adjascent_points(point: Point, risk_levels: list[list[Point]]) -> list[Point]:
+def get_adjascent_points(point: Point, points_grid: list[list[Point]]) -> list[Point]:
     return [
-        risk_levels[point.y + diff_y][point.x + diff_x]
+        points_grid[point.y + diff_y][point.x + diff_x]
         for diff_x, diff_y in ADJASCENT_DIFFS
-        if 0 <= (point.x + diff_x) < len(risk_levels[point.y]) and 0 <= (point.y + diff_y) < len(risk_levels)
+        if 0 <= (point.x + diff_x) < len(points_grid[point.y]) and 0 <= (point.y + diff_y) < len(points_grid)
     ]
 
 
-def find_least_risky_path(risk_levels: list[list[Point]]) -> Path:
-    start_point = risk_levels[0][0]
-    end_point = risk_levels[-1][-1]
-    least_risky_path_to_point: dict[Point, Path] = {
-        start_point: Path([start_point])
-    }
-    points_to_explore = deque(get_adjascent_points(start_point, risk_levels))
+def find_least_risky_path(points_grid: list[list[Point]]) -> PathPoint:
+    start_point = points_grid[0][0]
+    end_point = points_grid[-1][-1]
+    print(f"Finding path from {start_point} to {end_point}.")
 
-    while points_to_explore:
-        point = points_to_explore.popleft()
-        adjascent_points = get_adjascent_points(point, risk_levels)
-        adjascent_paths = [
-            least_risky_path_to_point[adjascent_point]
-            for adjascent_point in adjascent_points
-            if adjascent_point in least_risky_path_to_point and point not in least_risky_path_to_point[adjascent_point].points
-        ]
-        if adjascent_paths:
-            adjascent_paths.sort(key=lambda path: path.risk_level)
-            new_path_to_point = Path(adjascent_paths[0].points + [point])
-            if point not in least_risky_path_to_point or new_path_to_point.risk_level < least_risky_path_to_point[point].risk_level:
-                least_risky_path_to_point[point] = new_path_to_point
-                points_to_explore.extend(
-                    adjascent_point
-                    for adjascent_point in adjascent_points
-                    if adjascent_point not in new_path_to_point.points
-                )
+    start_path_point = PathPoint(start_point)
+    least_risky_path_to_point: dict[Point, PathPoint] = {
+        start_point: start_path_point
+    }
+    paths_to_explore: OrderedDict[Point, PathPoint] = OrderedDict({
+        adjascent_point: PathPoint(adjascent_point, start_path_point)
+        for adjascent_point in get_adjascent_points(start_point, points_grid)
+    })
+    explored_path_count = 0
+    ignored_local_suboptimal_path_count = 0
+    ignored_global_suboptimal_path_count = 0
+
+    while paths_to_explore:
+        point, path_point = paths_to_explore.popitem()
+
+        if point in least_risky_path_to_point and path_point.path_risk_level >= least_risky_path_to_point[point].path_risk_level:
+            ignored_local_suboptimal_path_count += 1
+            continue
+
+        if end_point in least_risky_path_to_point:
+            path_best_ultimate_risk_level = path_point.path_risk_level + (end_point.x - point.x) + (end_point.y - point.y)
+            if path_best_ultimate_risk_level >= least_risky_path_to_point[end_point].path_risk_level:
+                ignored_global_suboptimal_path_count += 1
+                continue
+
+        least_risky_path_to_point[point] = path_point
+        if point == end_point:
+            print(f"Found path to end with risk level {path_point.path_risk_level}.")
+
+        for adjascent_point in get_adjascent_points(point, points_grid):
+            if adjascent_point != path_point.previous_path_point.point:
+                new_path_point = PathPoint(adjascent_point, path_point)
+                if adjascent_point in paths_to_explore and new_path_point.path_risk_level >= paths_to_explore[adjascent_point].path_risk_level:
+                    ignored_local_suboptimal_path_count += 1
+                    continue
+                paths_to_explore[adjascent_point] = new_path_point
+                if adjascent_point.x < point.x or adjascent_point.y < point.y:
+                    paths_to_explore.move_to_end(adjascent_point, last=False)
+
+        explored_path_count += 1
+        if explored_path_count % 100000 == 0:
+            print(f"Explored {explored_path_count} paths.")
+
+    print(f"Explored {explored_path_count} paths.")
+    print(f"Ignored {ignored_local_suboptimal_path_count} locally suboptimal paths.")
+    print(f"Ignored {ignored_global_suboptimal_path_count} globally suboptimal paths.")
 
     return least_risky_path_to_point[end_point]
 
 
 if __name__ == '__main__':
     with open(FILE_PATH.parent / f'{FILE_PATH.stem}_input.txt') as file:
-        risk_levels = [
+        first_tile_points_grid = [
             [Point(x, y, int(risk_level_str)) for x, risk_level_str in enumerate(line.rstrip())]
             for y, line in enumerate(file)
         ]
 
-    least_risky_path = find_least_risky_path(risk_levels)
-    print(f"Least risky path:\n{least_risky_path}")
+    tile_height = len(first_tile_points_grid)
+    tile_width = len(first_tile_points_grid[0])
+    first_tile_least_risky_path = find_least_risky_path(first_tile_points_grid)
+    print(f"Least risky path for first {tile_width}x{tile_height} tile:\n{first_tile_least_risky_path}")
+
+    points_grid = [
+        [
+            Point(
+                x,
+                y,
+                (((first_tile_points_grid[y % tile_height][x % tile_width].risk_level + (x // tile_width) + (y // tile_height) - 1) % 9) + 1)
+            )
+            for x in range(5 * tile_width)
+        ]
+        for y in range(5 * tile_height)
+    ]
+    area_height = len(points_grid)
+    area_width = len(points_grid[0])
+    print(f"Full area is {area_width}x{area_height}.")
+
+    least_risky_path = find_least_risky_path(points_grid)
+    print(f"Least risky path for full {area_width}x{area_height} area:\n{least_risky_path}")
