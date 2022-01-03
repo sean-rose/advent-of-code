242f18b5c6f43225992f9b0a386da246f18161dc
Sean Rose <sean.s.rose@gmail.com>
2022-01-03 15:04:55 -0800

    Solve 2021 day 17 part 2.

--- 2021/day_17.py
+++ 2021/day_17.py
@@ -88,6 +88,49 @@ def fire_probe_at_target_area(initial_velocity: Velocity, target_area: Area) -> 
     return probe_positions
 
 
+def probe_would_hit_target_area(initial_velocity: Velocity, target_area: Area) -> bool:
+    probe = Probe(Point(0, 0), initial_velocity)
+    step = 0
+    while (
+        not target_area.contains_point(probe.position)
+        and probe.position.x <= target_area.bottom_right.x
+        and probe.position.y >= target_area.bottom_right.y
+    ):
+        step += 1
+        probe.move()
+
+    if target_area.contains_point(probe.position):
+        print(f"Firing probe with velocity {initial_velocity} would hit target {target_area} at {probe.position} after {step} steps.")
+        return True
+    else:
+        return False
+
+
+def aim_all(target_area: Area) -> list[Velocity]:
+    min_x_velocity = 0
+    projected_x_distance = 0
+    while projected_x_distance < target_area.top_left.x:
+        min_x_velocity += 1
+        projected_x_distance += min_x_velocity
+    max_x_velocity = target_area.bottom_right.x
+
+    min_y_velocity = target_area.bottom_right.y
+    max_y_velocity = abs(target_area.bottom_right.y) - 1
+
+    print(f"Possible initial x velocity:  {min_x_velocity}..{max_x_velocity}")
+    print(f"Possible initial y velocity:  {min_y_velocity}..{max_y_velocity}")
+    print(f"Possible initial volicity count:  {(max_x_velocity - min_x_velocity + 1) * (max_y_velocity - min_y_velocity + 1)}")
+    return [
+        velocity
+        for velocity in (
+            Velocity(x, y)
+            for x in range(min_x_velocity, max_x_velocity + 1)
+            for y in range(min_y_velocity, max_y_velocity + 1)
+        )
+        if probe_would_hit_target_area(velocity, target_area)
+    ]
+
+
 if __name__ == '__main__':
     with open(FILE_PATH.parent / f'{FILE_PATH.stem}_input.txt') as file:
         line = file.readline().rstrip()
@@ -100,3 +143,6 @@ if __name__ == '__main__':
     probe_positions = fire_probe_at_target_area(initial_velocity, target_area)
     max_probe_height = max(position.y for position in probe_positions)
     print(f"Max probe height:  {max_probe_height}")
+
+    initial_velocities = aim_all(target_area)
+    print(f"Successful initial velocity count:  {len(initial_velocities)}")
